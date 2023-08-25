102879bee953534e9c54d90b54fb1658766510e0
salihcelebi96 <celebisalih277@gmail.com>
2023-08-25 03:56:09 +0300

    9.commit

--- src/components/card.js
+++ src/components/card.js
@@ -16,7 +16,7 @@ const ProjectGallery = () => {
 
   return (
     <div>
-      <div className="project-list grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mx-3 my-5">
+      <div className="project-list overflow-hidden  grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mx-3 my-5">
         {screenData.map((project, index) => (
           <Card
             key={index}
@@ -33,31 +33,28 @@ const ProjectGallery = () => {
                 </CardBody>
                 <a className='' href={project.url} target="_blank" rel="noopener noreferrer">
                   <img className='h-full w-full py-5 ' src={project.image} alt={`Proje ${index}`} />
-                  {showInfo === index && (
-                    <div className={`hv transition-transform duration-300 gap-5  ease-in-out absolute inset-0 bg-gray-800 bg-opacity-90 h-full  flex flex-col items-center justify-center px-5  text-xl  ${showInfo === index ? 'h-full' : ''}`}>
-                      <div className='flex gap-3'>
-                        <div>Tools and Styled :</div>
-                        <div>{project.tools}</div>
-                      </div>
-
-                      <div>
-                        {project.info}
-                      </div>
-                      <div className='flex  justify-center items-center  mt-10  gap-5 '>
-                        <div className='  flex justify-center items-center'>
-                          <Link to={project.url} target="_blank" rel="noopener noreferrer" className="text-white text-sm py-2 px-2 border-none rounded-md bg-green-600  hover:bg-green-800  "  >
-                            Learn More
-                          </Link>
-                        </div>
-                        <div className='flex justify-center items-center' >
-                          <Link to={project.sourceCode} target="_blank" rel="noopener noreferrer" className='text-sm py-2 px-2 bg-gray-400 hover:bg-gray-600 border-none rounded-md '>
-                            Source Code 
-                          </Link>
-                        </div>
-                      </div>
-
+                  <div id='hoverCard' className={`transition-transform ${showInfo === index ? 'translate-y-0  ' : 'translate-y-full '} duration-1000 gap-5 border-none rounded-lg ease-in-out absolute inset-0 bg-gray-800 bg-opacity-90 h-full flex flex-col items-center justify-center px-5 text-xl`} style={{ visibility: showInfo === index ? 'visible' : 'hidden' }}>
+                    <div className='flex gap-3'>
+                      <div>Tools and Styled :</div>
+                      <div>{project.tools}</div>
                     </div>
-                  )}
+
+                    <div>
+                      {project.info}
+                    </div>
+                    <div className='flex justify-center items-center mt-10 gap-5'>
+                      <div className='flex justify-center items-center'>
+                        <Link to={project.url} target="_blank" rel="noopener noreferrer" className="text-white text-sm py-2 px-2 border-none rounded-md bg-green-600 hover:bg-green-800">
+                          Learn More
+                        </Link>
+                      </div>
+                      <div className='flex justify-center items-center'>
+                        <Link to={project.sourceCode} target="_blank" rel="noopener noreferrer" className='text-sm py-2 px-2 bg-gray-400 hover:bg-gray-600 border-none rounded-md'>
+                          Source Code
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
                 </a>
               </div>
             </CardHeader>
