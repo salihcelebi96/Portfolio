efd235ed9fc66d91cb01bf8e926352dd15c008cc
salihcelebi96 <celebisalih277@gmail.com>
2023-08-30 19:55:35 +0300

    4.commit

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
           <Link className='hover:bg-gray-600  hover:text-white flex items-center px-4 h-full' to="/">Home</Link>
         </li>
         <li className='h-full'>
-          <Link className='hover:bg-gray-600 hover:text-white flex items-center px-4 h-full' to="/projects">Projects</Link>
+          <Link className='hover:bg-gray-600 hover:text-white flex items-center  h-full' to="/projects">Projects</Link>
         </li>
         <li className='h-full'>
           <Link className='hover:bg-gray-600 hover:text-white flex items-center px-4 h-full' to="/contact">Contact</Link>
