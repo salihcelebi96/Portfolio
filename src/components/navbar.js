04187f53bea214f5c9151b75f906f70ca07cbd43
salihcelebi96 <celebisalih277@gmail.com>
2023-08-30 19:59:11 +0300

    5.commit

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className='h-16 flex items-center    bg-gray-200 text-black text-2xl'>
+    <nav className='h-16 flex items-center  w-full  bg-gray-200 text-black text-2xl'>
       <ul className='flex gap-10   h-full'>
         <li className='h-full'>
           <Link className='hover:bg-gray-600  hover:text-white flex items-center px-4 h-full' to="/">Home</Link>
