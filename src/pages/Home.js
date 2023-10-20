8c9e75e5a33170e13725d4c620d618ab69bf9dfa
salihcelebi96 <celebisalih277@gmail.com>
2023-10-20 03:36:42 +0300

    10.commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,19 +3,23 @@ import background from "../assets/earth.jpg"
 import Navbar  from '../components/navbar'
 import Me from "../components/me"
 import Work from "../components/work";
+import "../css/Home.css";
 
 const Home = () => {
   return (
     <div>
-      <div className='h-full'>
+      <div className='h-full  '>
         <div>
-            <img  src={background} alt=""/>
+        <div className=''>
+            <img className='topping' src={background} alt=""/>
         </div>
-        <div className='content absolute lg:text-3xl xl:text-5xl  md:text-lg   sm:text-xl   top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white '>
-          <p>Hello</p>
-          <p className='pt-2 pb-6 '>Welcome to My Web Page</p>
-          <p>On this page, you'll find everything you need to know about me and my work.</p>
+        <div className='content absolute  sm:text-4xl  md:text-4xl   top-1/3 left-1/2   transform -translate-x-1/2 -translate-y-1/2   text-center  text-white '>
+          <p className='hello '>Hello</p>
+          <p className='pt-2 pb-6 welcome'>Welcome to My Web Page</p>
+          <p className='bolder upword'>On this page, you'll find everything you need to know about me and my work.</p>
         </div>
+        </div>
+        
         <div className=' sticky top-0 z-10'>
           <Navbar/>
         </div>
